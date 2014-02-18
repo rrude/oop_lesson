359f702303665e8b48a248488c4b215a9d5297e2
Hackbright Student <user@mini-006.(none)>
2014-02-18 12:11:55 -0800

    Finished step 3.

--- game.py
+++ game.py
@@ -11,13 +11,39 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 4
+GAME_HEIGHT = 4
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
+
+
+
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    #Initialize and register rock 1
+    rock1 = Rock()
+    GAME_BOARD.register(rock1)
+    GAME_BOARD.set_el(1, 1, rock1)
+
+
+    #Initialize and register rock 2
+    rock2 = Rock()
+    GAME_BOARD.register(rock2)
+    GAME_BOARD.set_el(2, 2, rock2)
+
+    #Initialize and register rock 3
+    rock3 = Rock()
+    GAME_BOARD.register(rock3)
+    GAME_BOARD.set_el(3, 3, rock3)
+
+
+    print "The first rock is at", (rock1.x, rock1.y)
+    print "The second rock is at", (rock2.x, rock2.y)
+    print "The third rock is at", (rock3.x, rock3.y)
+    print "Rock 1 image", rock1.IMAGE
+    print "Rock 2 image", rock2.IMAGE
+    print "Rock 3 image", rock3.IMAGE 
